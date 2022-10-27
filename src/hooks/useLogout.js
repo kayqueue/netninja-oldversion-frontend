3a671828271ed21523b89d6de529d22338b79e22
kayqueue <kwanquantan@gmail.com>
2022-10-28 00:10:45 +0800

    modified:   src/hooks/useLogout.js

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,9 +1,10 @@
 import { useAuthContext } from "./useAuthContext"
-
+import { useWorkoutsContext } from "./useWorkoutsContext"
 
 
 export const useLogout = () => {
     const { dispatch } = useAuthContext()
+    const { dispatch: workoutsDispatch } = useWorkoutsContext()
 
     const logout = () => {
         // remove user from storage
@@ -11,6 +12,7 @@ export const useLogout = () => {
 
         // dispatch logout action
         dispatch({type: 'LOGOUT'}) // no payload - just reset the user = null in useAuthContext.js
+        workoutsDispatch({ type: 'SET_WORKOUTS', payload: null }) // clear global state - prevent the previous state from flashing
     }
 
     return { logout }
